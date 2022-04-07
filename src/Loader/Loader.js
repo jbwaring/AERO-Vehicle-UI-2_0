8bfb8447efd6e7d7330dcad13da35ab1c0e53c11
jbwaring <jean-baptiste@zetane.com>
2022-04-07 17:10:43 -0400

    bug fixes

--- src/Loader/Loader.js
+++ src/Loader/Loader.js
@@ -6,7 +6,8 @@ import loaderBackground from '/public/Loader-Background.svg'
 import homeBackground from '/public/home-background.svg'
 // const webSocketURL = 'ws://192.168.2.46:9001'
 // const webSocketURL = 'ws://172.31.62.29:8086'
-const webSocketURL = 'ws://172.31.107.52:9001'
+// const webSocketURL = 'ws://172.31.107.52:9001'
+const webSocketURL = 'ws://172.31.78.125:9001'
 
 const Loader = () => {
 
